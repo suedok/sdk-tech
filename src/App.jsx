e4b085317b2be625ecc85947f21b6403a9eda055
Sued Silva <suedok@gmail.com>
2025-08-29 07:53:18 -0400

    Primeiro commit do SDK Tech

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './style.css'
+
+function App() {
+  return (
+    <div className="container">
+      <h1>SDK Tech ⚡</h1>
+      <p>Energia em suas mãos.</p>
+    </div>
+  )
+}
+
+export default App
